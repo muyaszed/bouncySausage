5fdff4af4a5f96d7c8686f1e333644c6d074ee2a
Yazed Jamal <muyaszed@gmail.com>
2018-08-01 09:26:48 +0100

    first commit

---
+++ src/game/Game.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.awt.Canvas;
+
+/**
+ *
+ * @author muyaszed
+ */
+public class Game {
+    
+    public final static int WIDTH = 800, HEIGHT = 600;
+    
+    private String gameName = "Bouncy Sausage";
+    
+    private Canvas game = new Canvas();
+    
+    public void start() {
+        //init windows
+        
+        
+        //Game loop
+        boolean running = true;
+        while(running) {
+            
+        }
+        
+        //Game end
+    }
+    
+}
